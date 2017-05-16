0e4ab06eac53dedd0619eb7e91bd8f89c89d5147
Arthur Renaud <arthurrenaud.pro@gmail.com>
2017-05-16 16:37:32 +0200

    operational views, routes and actions + some basic css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,33 +2,42 @@ class ArticlesController < ApplicationController
   before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
+    @articles = Article.all
   end
 
   def show
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create(article_params)
+    # redirect_to "/restaurants/#{@restaurant.id}"
+    redirect_to article_path(@article)
   end
 
   def edit
   end
 
   def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
 
   def find_task
-    @task = Task.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def task_params
-    params.require(:task).permit(:name, :content, :done)
+    params.require(:article).permit(:title, :content)
   end
 end
